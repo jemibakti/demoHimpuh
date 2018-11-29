0dcc10ef01bb00e9f621938f2c832d8fb1619470
jemibakti <jhee.ananda@gmail.com>
2018-11-29 15:45:19 +0700

    tes

--- application/views/element/head_excell.php
+++
@@ -1,7 +0,0 @@
-
-<?php 
-	header("Content-type: application/octet-stream"); 
-	header('Content-Disposition: attachment; filename="'.$filename.'.xls"');//ganti nama sesuai keperluan 
-	header("Pragma: no-cache"); 
-	header("Expires: 0"); 
-?>
